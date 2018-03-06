bd6611eca89455969fbb720342bec93d40611c14
ywCN <wangyouhao90@gmail.com>
2018-03-05 20:35:52 -0500

    refactored orginal module

--- GetRepo.py
+++ GetRepo.py
@@ -7,30 +7,26 @@ def get_repo_info():
     res = requests.get(user_url)
     return json.loads(res.text)
 
-def get_commit_info(repo_name):
-    repo_url = 'https://api.github.com/repos/ywang567/{}/commits'.format(repo_name)
-    repo_info = requests.get(repo_url)
-    return json.loads(repo_info.text)
+def get_commit_info(repos):
+    commit_info = []
+    for repo in repos:
+        repo_name = repo['name']
+        repo_url = 'https://api.github.com/repos/ywang567/{}/commits'.format(repo_name)
+        repo_info = requests.get(repo_url)
+        repo_info_json = json.loads(repo_info.text)
+        commit_info.append('Repo: {} Number of commits: {}'.format(repo_name, len(repo_info_json)))
+
+    return commit_info
 
 def get_user_info():
     user_info = []
-    repos = get_repo_info()
-    print('repos are', repos)
     user_info.append('User: ywang567')
 
-    try:
-        repos[0]['name']
-    except (TypeError, KeyError, IndexError):
-        return 'unable to fetch repos from user'
+    commit_info = get_commit_info(get_repo_info())
 
-    for repo in repos:
-        repo_info_json = get_commit_info(repo['name']);
-        # print(repo_info_json)
-        user_info.append('Repo: {} Number of commits: {}'.format(repo['name'], len(repo_info_json)))
-
-    return user_info
+    return user_info + commit_info
 
 
 if __name__ == '__main__':
-    for entry in get_repo_info():
+    for entry in get_user_info():
         print(entry)
